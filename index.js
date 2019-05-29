758b728eb572f95c5cc1eb7e93b8f63afc67e4a5
Abhilash Konnur <abhilashkonnur1@gmail.com>
2019-05-29 17:04:05 +0530

    10.14

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+ // res.send('Hello abhilash branch World!');
+ 
+ 
+
+res.send("Hello world Linux Abhilash 10.14");
+
+
+
+ 
+});
+
+var port = process.env.PORT || 1337;
+var server = app.listen(port,function(){
+	
+});
+
+console.log("Server running at http://localhost:%d", port);
