e1c86bac05d6834b656af01efe6e31c3e05afad5
Abhilash Konnur <abhilashkonnur1@gmail.com>
2019-07-15 16:36:03 +0530

    winston

--- index.js
+++ index.js
@@ -18,5 +18,35 @@ var port = process.env.PORT || 1337;
 var server = app.listen(port,function(){
 	
 });
+var winston = require(“winston”);
 
+require(“winston-azure-blob-transport”);
+
+var logger = new (winston.Logger)({
+
+    transports: [
+
+      new (winston.transports.AzureBlob)({
+
+        account: {
+
+          name: process.env.ACCOUNT_NAME,
+
+          key: process.env.ACCOUNT_KEY
+
+        },
+
+        containerName: process.env.CONTAINER_NAME,
+
+        blobName: “test.log“,
+
+        level: “info”
+
+      })
+
+    ]
+
+  });
+
+logger.info(“Hello!”);
 console.log("Server running at http://localhost:%d", port);
